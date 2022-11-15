ff817ffc508ab1801a312c05fb2a42af1eb5c3c3
Lukas Paur <lukas.paur@i.cz>
2022-11-15 19:25:48 +0100

    [NO-TICKET] Modify res test

--- app_dir/core/pagination.py
+++ app_dir/core/pagination.py
@@ -10,6 +10,7 @@ class PostLimitOffsetPagination(PageNumberPagination, LimitOffsetPagination):
     def get_paginated_response(self, data):
         return Response({
             'links': {
+                'boo': 'hoo',
                 'next': self.get_next_link(),
                 'previous': self.get_previous_link()
             },
